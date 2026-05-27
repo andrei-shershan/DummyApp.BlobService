6c921747d8c8e8b6d29855e9732a9bad25bebba9
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2026-05-28 01:17:20 +0200

    Use blobStorageUri frok tf (#6)

--- src/DummyApp.BlobService.Functions/Program.cs
+++ src/DummyApp.BlobService.Functions/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using Azure.Storage.Blobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +11,15 @@ var host = new HostBuilder()
         services.AddSingleton(sp =>
         {
             var configuration = sp.GetRequiredService<IConfiguration>();
+            var blobStorageUri = configuration["BlobStorageUri"];
+
+            if (!string.IsNullOrEmpty(blobStorageUri))
+            {
+                // Deployed: use managed identity
+                return new BlobServiceClient(new Uri(blobStorageUri), new DefaultAzureCredential());
+            }
+
+            // Local: fall back to connection string (UseDevelopmentStorage=true)
             var connectionString = configuration["AzureWebJobsStorage"] ?? string.Empty;
             return new BlobServiceClient(connectionString);
         });
