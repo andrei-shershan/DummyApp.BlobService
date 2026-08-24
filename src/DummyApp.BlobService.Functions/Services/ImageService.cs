51a7afc83dd76245d863cb884149b363c26cf18f
Andrei Shershan <139045906+andrei-shershan@users.noreply.github.com>
2026-08-25 01:20:03 +0200

    Upload avatar (#16)

--- src/DummyApp.BlobService.Functions/Services/ImageService.cs
+++ src/DummyApp.BlobService.Functions/Services/ImageService.cs
@@ -19,24 +19,56 @@ public sealed class ImageService : IImageService
 
     public async Task<ImageUploadResult> ProcessAndUploadAsync(string fileName, byte[] imageBytes, string contentType, ImageType imageType, CancellationToken cancellationToken)
     {
+        using var image = Image.Load(imageBytes);
+        var format = image.Metadata.DecodedImageFormat ?? throw new InvalidOperationException("Unable to determine image format.");
+
+        if (imageType == ImageType.Avatar)
+        {
+            var avatarImage = image.Clone(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(250, 250),
+                Mode = ResizeMode.Crop,
+                Position = AnchorPositionMode.Center
+            }));
+
+            await using var avatarStream = new MemoryStream();
+            await avatarImage.SaveAsync(avatarStream, format, cancellationToken);
+            var avatarBytes = avatarStream.ToArray();
+            var avatarUri = await _blobStorageService.UploadAsync(imageType, fileName, avatarBytes, contentType, cancellationToken);
+
+            var thumbnailImage = image.Clone(x => x.Resize(new ResizeOptions
+            {
+                Size = new Size(100, 100),
+                Mode = ResizeMode.Crop,
+                Position = AnchorPositionMode.Center
+            }));
+
+            await using var thumbnailStream = new MemoryStream();
+            await thumbnailImage.SaveAsync(thumbnailStream, format, cancellationToken);
+            var thumbnailBytes = thumbnailStream.ToArray();
+
+            var thumbnailFileName = GetSmallFileName(fileName);
+            var thumbnailUri = await _blobStorageService.UploadAsync(imageType, thumbnailFileName, thumbnailBytes, contentType, cancellationToken);
+
+            return new ImageUploadResult(avatarUri, thumbnailUri);
+        }
+
         var originalUri = await _blobStorageService.UploadAsync(imageType, fileName, imageBytes, contentType, cancellationToken);
 
-        using var image = Image.Load(imageBytes);
         image.Mutate(x => x.Resize(new ResizeOptions
         {
             Size = new Size(250, 0),
             Mode = ResizeMode.Max
         }));
 
-        await using var thumbnailStream = new MemoryStream();
-        var format = image.Metadata.DecodedImageFormat ?? throw new InvalidOperationException("Unable to determine image format.");
-        await image.SaveAsync(thumbnailStream, format, cancellationToken);
-        var thumbnailBytes = thumbnailStream.ToArray();
+        await using var smallStream = new MemoryStream();
+        await image.SaveAsync(smallStream, format, cancellationToken);
+        var smallBytes = smallStream.ToArray();
 
-        var thumbnailFileName = GetSmallFileName(fileName);
-        var thumbnailUri = await _blobStorageService.UploadAsync(imageType, thumbnailFileName, thumbnailBytes, contentType, cancellationToken);
+        var smallFileName = GetSmallFileName(fileName);
+        var smallUri = await _blobStorageService.UploadAsync(imageType, smallFileName, smallBytes, contentType, cancellationToken);
 
-        return new ImageUploadResult(originalUri, thumbnailUri);
+        return new ImageUploadResult(originalUri, smallUri);
     }
 
     private static string GetSmallFileName(string fileName)
